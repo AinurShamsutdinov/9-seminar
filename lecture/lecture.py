2a2fa5356ff16a13f3cae81804898abd3a83d2df
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-22 13:25:20 +0300

    lecture

--- lecture/lecture.py
+++ lecture/lecture.py
@@ -213,35 +213,65 @@
 #
 # main('test')
 ##################################################################################
+# from typing import Callable
+#
+#
+# def cache(func: Callable):
+#     _cache_dict = {}
+#
+#     def wrapper(*args):
+#         if args not in _cache_dict:
+#             _cache_dict[args] = func(*args)
+#         return _cache_dict[args]
+#     return wrapper
+#
+#
+# @cache
+# def factorial(n: int) -> int:
+#     print(f'Вычисляю факториал для числа {n}')
+#     f = 1
+#     for i in range(2, n + 1):
+#         f *= i
+#     return f
+#
+#
+# print(f'{factorial(10) = }')
+# print(f'{factorial(15) = }')
+# print(f'{factorial(10) = }')
+# print(f'{factorial(20) = }')
+# print(f'{factorial(10) = }')
+# print(f'{factorial(20) = }')
+##################################################################################
+import time
 from typing import Callable
 
 
-def cache(func: Callable):
-    _cache_dict = {}
+def count(num: int = 1):
+    def deco(func: Callable):
+        def wrapper(*args, **kwargs):
+            time_for_count = []
+            result = None
+            for _ in range(num):
+                start = time.perf_counter()
+                result = func(*args, **kwargs)
+                stop = time.perf_counter()
+                time_for_count.append(stop - start)
+            print(f'Результаты замеров {time_for_count}')
+            return result
+        return wrapper
+    return deco
 
-    def wrapper(*args):
-        if args not in _cache_dict:
-            _cache_dict[args] = func(*args)
-        return _cache_dict[args]
-    return wrapper
 
-
-@cache
+@count(10)
 def factorial(n: int) -> int:
-    print(f'Вычисляю факториал для числа {n}')
     f = 1
     for i in range(2, n + 1):
         f *= i
     return f
 
 
-print(f'{factorial(10) = }')
-print(f'{factorial(15) = }')
-print(f'{factorial(10) = }')
-print(f'{factorial(20) = }')
-print(f'{factorial(10) = }')
-print(f'{factorial(20) = }')
-##################################################################################
+print(f'{factorial(1000) = }')
+print(f'{factorial(1000) = }')
 ##################################################################################
 ##################################################################################
 ##################################################################################
