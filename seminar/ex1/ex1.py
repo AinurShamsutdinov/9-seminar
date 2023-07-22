3ad7ddf4596f2ee7700eef2fd8e5aff1994f988e
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-22 15:01:25 +0300

    ex1 solution

--- seminar/ex1/ex1.py
+++ seminar/ex1/ex1.py
@@ -3,4 +3,25 @@
 # ○ от 1 до 100 для загадывания,
 # ○ от 1 до 10 для количества попыток
 # 📌 Функция возвращает функцию, которая через консоль просит угадать загаданное число за указанное число попыток.
+from typing import Callable
 
+
+def guess() -> Callable:
+    num: int = int(input('Enter number between 1 and 100: '))
+    tries: int = int(input('Enter amount tries between 1 and 10: '))
+
+    def input_num():
+        nonlocal num
+        nonlocal tries
+        guess_num = -1
+        for _ in range(tries):
+            if guess_num != num:
+                guess_num = int(input("Enter your guess: "))
+            elif guess_num == num:
+                print(f'Bingo the number is {num}')
+        if guess_num != num:
+            print('Better luck another time')
+    return input_num()
+
+
+guess_func = guess()
