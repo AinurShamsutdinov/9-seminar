567ec7adebafda8a4fb2ca5c7e459761fffc3ece
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-22 12:09:47 +0300

    lecture

--- lecture/lecture.py
+++ lecture/lecture.py
@@ -156,63 +156,91 @@
 #
 # print(f'{factorial(1000) = }')
 ##################################################################################
+# from typing import Callable
+#
+#
+# def deco_a(func: Callable):
+#     def wrapper_a(*args, **kwargs):
+#         print('Старт декоратора A')
+#         print(f'Запускаю {func.__name__}')
+#         res = func(*args, **kwargs)
+#         print(f'Завершение декоратора A')
+#         return res
+#     print('Возвращаем декоратор A')
+#     return wrapper_a
+#
+#
+# def deco_b(func: Callable):
+#     def wrapper_b(*args, **kwargs):
+#         print('Старт декоратора B')
+#         print(f'Запускаю {func.__name__}')
+#         res = func(*args, **kwargs)
+#         print(f'Завершение декоратора B')
+#         return res
+#     print('Возвращаем декоратор B')
+#     return wrapper_b
+#
+#
+# def deco_c(func: Callable):
+#     def wrapper_c(*args, **kwargs):
+#         print('Старт декоратора C')
+#         print(f'Запускаю {func.__name__}')
+#         res = func(*args, **kwargs)
+#         print(f'Завершение декоратора C')
+#         return res
+#     print('Возвращаем декоратор C')
+#     return wrapper_c
+#
+#
+# def deco_d(func: Callable):
+#     def wrapper_d(*args, **kwargs):
+#         print('Старт декоратора D')
+#         print(f'Запускаю {func.__name__}')
+#         res = func(*args, **kwargs)
+#         print(f'Завершение декоратора D')
+#         return res
+#     print('Возвращаем декоратор D')
+#     return wrapper_d
+#
+#
+# @deco_d
+# @deco_c
+# @deco_b
+# @deco_a
+# def main(text):
+#     print('Старт основной функции ', text)
+#
+#
+# main('test')
+##################################################################################
 from typing import Callable
 
 
-def deco_a(func: Callable):
-    def wrapper_a(*args, **kwargs):
-        print('Старт декоратора A')
-        print(f'Запускаю {func.__name__}')
-        res = func(*args, **kwargs)
-        print(f'Завершение декоратора A')
-        return res
-    print('Возвращаем декоратор A')
-    return wrapper_a
+def cache(func: Callable):
+    _cache_dict = {}
+
+    def wrapper(*args):
+        if args not in _cache_dict:
+            _cache_dict[args] = func(*args)
+        return _cache_dict[args]
+    return wrapper
 
 
-def deco_b(func: Callable):
-    def wrapper_b(*args, **kwargs):
-        print('Старт декоратора B')
-        print(f'Запускаю {func.__name__}')
-        res = func(*args, **kwargs)
-        print(f'Завершение декоратора B')
-        return res
-    print('Возвращаем декоратор B')
-    return wrapper_b
+@cache
+def factorial(n: int) -> int:
+    print(f'Вычисляю факториал для числа {n}')
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
 
 
-def deco_c(func: Callable):
-    def wrapper_c(*args, **kwargs):
-        print('Старт декоратора C')
-        print(f'Запускаю {func.__name__}')
-        res = func(*args, **kwargs)
-        print(f'Завершение декоратора C')
-        return res
-    print('Возвращаем декоратор C')
-    return wrapper_c
-
-
-def deco_d(func: Callable):
-    def wrapper_d(*args, **kwargs):
-        print('Старт декоратора D')
-        print(f'Запускаю {func.__name__}')
-        res = func(*args, **kwargs)
-        print(f'Завершение декоратора D')
-        return res
-    print('Возвращаем декоратор D')
-    return wrapper_d
-
-
-@deco_d
-@deco_c
-@deco_b
-@deco_a
-def main(text):
-    print('Старт основной функции ', text)
-
-
-main('test')
-##################################################################################
+print(f'{factorial(10) = }')
+print(f'{factorial(15) = }')
+print(f'{factorial(10) = }')
+print(f'{factorial(20) = }')
+print(f'{factorial(10) = }')
+print(f'{factorial(20) = }')
 ##################################################################################
 ##################################################################################
 ##################################################################################
