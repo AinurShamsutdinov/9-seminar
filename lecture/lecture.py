e421fe03fb658e98f5fe37c17b9e5880063640f6
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-22 13:50:13 +0300

    lecture

--- lecture/lecture.py
+++ lecture/lecture.py
@@ -242,52 +242,88 @@
 # print(f'{factorial(10) = }')
 # print(f'{factorial(20) = }')
 ##################################################################################
-import time
-from typing import Callable
+# import time
+# from typing import Callable
+#
+#
+# def count(num: int = 1):
+#     def deco(func: Callable):
+#         def wrapper(*args, **kwargs):
+#             time_for_count = []
+#             result = None
+#             for _ in range(num):
+#                 start = time.perf_counter()
+#                 result = func(*args, **kwargs)
+#                 stop = time.perf_counter()
+#                 time_for_count.append(stop - start)
+#             print(f'Результаты замеров {time_for_count}')
+#             return result
+#         return wrapper
+#     return deco
+#
+#
+# @count(10)
+# def factorial(n: int) -> int:
+#     f = 1
+#     for i in range(2, n + 1):
+#         f *= i
+#     return f
+#
+#
+# print(f'{factorial(1000) = }')
+# print(f'{factorial(1000) = }')
+##################################################################################
+# import time
+# from functools import wraps
+# from typing import Callable
+#
+#
+# def count(num: int = 1):
+#     def deco(func: Callable):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             time_for_count = []
+#             result = None
+#             for _ in range(num):
+#                 start = time.perf_counter()
+#                 result = func(*args, **kwargs)
+#                 stop = time.perf_counter()
+#                 time_for_count.append(stop - start)
+#             print(f'Результаты замеров {time_for_count}')
+#             return result
+#         return wrapper
+#     return deco
+#
+#
+# @count(10)
+# def factorial(n: int) -> int:
+#     """Returns the factorial of the number n."""
+#     f =1
+#     for i in range(2, n + 1):
+#         f *= i
+#         return f
+#
+#
+# print(f'{factorial(1000) = }')
+# print(f'{factorial.__name__ = }')
+# help(factorial)
+##################################################################################
+from functools import cache
 
 
-def count(num: int = 1):
-    def deco(func: Callable):
-        def wrapper(*args, **kwargs):
-            time_for_count = []
-            result = None
-            for _ in range(num):
-                start = time.perf_counter()
-                result = func(*args, **kwargs)
-                stop = time.perf_counter()
-                time_for_count.append(stop - start)
-            print(f'Результаты замеров {time_for_count}')
-            return result
-        return wrapper
-    return deco
-
-
-@count(10)
+@cache
 def factorial(n: int) -> int:
+    print(f'Вычисляю факториал для числа {n}')
     f = 1
     for i in range(2, n + 1):
         f *= i
-    return f
+        return f
 
 
-print(f'{factorial(1000) = }')
-print(f'{factorial(1000) = }')
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
-##################################################################################
+print(f'{factorial(10) = }')
+print(f'{factorial(15) = }')
+print(f'{factorial(10) = }')
+print(f'{factorial(20) = }')
+print(f'{factorial(10) = }')
+print(f'{factorial(20) = }')
 ##################################################################################
