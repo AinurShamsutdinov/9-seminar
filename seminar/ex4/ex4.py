c13fc1e72de034087783c097eaf86f811b3254f4
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-23 10:10:00 +0300

    ex4 solution

--- seminar/ex4/ex4.py
+++ seminar/ex4/ex4.py
@@ -1,4 +1,22 @@
 # Задание No4
 # 📌 Создайте декоратор с параметром.
 # 📌 Параметр - целое число, количество запусков декорируемой функции.
+from typing import Callable
 
+
+def decorator(func: Callable):
+    repeat: int = 5
+
+    def wrapper(*args, **kwargs):
+        nonlocal repeat
+        for i in range(repeat):
+            func(*args, **kwargs)
+    return wrapper
+
+
+@decorator
+def test_func(param) -> int:
+    print(f'decorated function call with {param = }')
+
+
+test_func('test')
